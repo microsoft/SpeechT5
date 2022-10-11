7f97fc4a6e28f6bc08053a1b006a772aa1300ad9
zz12375 <zz12375@mail.ustc.edu.cn>
2022-10-11 12:50:44 -0700

    update data preparation code for SpeechLM

--- SpeechLM/speechlm/data_process/phoneme_tokenizer/ltr2kaldi_phn_sil025.py
+++ SpeechLM/speechlm/data_process/phoneme_tokenizer/ltr2kaldi_phn_sil025.py
@@ -1,16 +1,33 @@
+# ----------------------------------------------------------------------------
+# SpeechLM: Enhanced Speech Pre-Training with Unpaired Textual Data (https://arxiv.org/abs/2209.15329)
+# Github source: https://github.com/microsoft/SpeechT5/tree/main/SpeechLM
+# Code based on fairseq: https://github.com/facebookresearch/fairseq/tree/272c4c5197250997148fb12c0db6306035f166a4
+# 
+# Copyright (c) 2022 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# ----------------------------------------------------------------------------
+
+import os
 import tqdm
 import argparse
 import numpy as np
 
-sil_prob = 0.25
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", "-i", required=True, type=str)
 parser.add_argument("--output", "-o", required=True, type=str)
-parser.add_argument("--lexicon", default='/stdblob/users/v-sanych/data/librispeech/librispeech_lm_norm_phn2char/kaldi/lang_nosp/phones/align_lexicon.txt', type=str)
+parser.add_argument("--lexicon", default='align_lexicon.txt', type=str)
 args = parser.parse_args()
 
+sil_prob = 0.25
+
+if not os.path.exists(args.lexicon):
+    print(f"| Warning: lexicon {args.lexicon} not found, downloading ...")
+    try:
+        os.system(f"wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1QVeyCpLXLnujBUAickpo-jaSVY-vKLnT' -O {args.lexicon}")
+    except Exception as e:
+        print(e)
+        print(f"| Error downloading {args.lexicon}, please download it from https://drive.google.com/file/d/1QVeyCpLXLnujBUAickpo-jaSVY-vKLnT/view?usp=sharing")
+        exit(1)
 dict = {}
 f = open(args.lexicon)
 for l in f:
