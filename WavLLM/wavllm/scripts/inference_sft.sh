e63df48de06fc2f85157dc8ad775c2a963d1ec92
XiaoshanHsj <1191315155@qq.com>
2024-03-22 11:11:25 +0000

    Add fairseq as a submodule

--- WavLLM/wavllm/scripts/inference_sft.sh
+++ WavLLM/wavllm/scripts/inference_sft.sh
@@ -9,22 +9,21 @@ src_dir=${model_path%/*}
 cpt=${model_path##*/}
 cpt=${cpt%.*}
 
-beam_size=$2
-gen_set=$3
+gen_set=$2
 [ -z $gen_set ] && gen_set="?"
 [ -z $beam_size ] && beam_size=1
 
 
 FAIRSEQ_ROOT=${PWD}
-DATA_DIR=$FAIRSEQ_ROOT/../wavllm/test_data
+DATA_DIR=$FAIRSEQ_ROOT/examples/wavllm/test_data
 
 for subset in $gen_set; do
     results_path=$src_dir/decode_${cpt}_beam${beam_size}/${subset}
     [ ! -d $results_path ] && mkdir -p $results_path
 
-    python $FAIRSEQ_ROOT/../wavllm/inference/generate.py $DATA_DIR \
-    --user-dir ../wavllm \
-    --tokenizer-path $FAIRSEQ_ROOT/../wavllm/tokenizer/tokenizer.model \
+    python $FAIRSEQ_ROOT/examples/wavllm/inference/generate.py $DATA_DIR \
+    --user-dir examples/wavllm \
+    --tokenizer-path $FAIRSEQ_ROOT/examples/wavllm/tokenizer/tokenizer.model \
     --gen-subset ${subset} \
     \
     --task speechllm_task \
@@ -37,16 +36,4 @@ for subset in $gen_set; do
     --max-tokens 1600000 \
     --sampling --beam 1 --nbest 1 --temperature 0.5 \
     --max-len-a 0 --max-len-b 512
-    # --beam ${beam_size} \
-    #
-    
-
-    # echo $results_path
-    # tail -n 1 $results_path/generate-*.txt
-    # sleep 1s
 done
-
-    #--max-sample-size 2000000 \
-    # --max-len-a 0 --max-len-b 512 \
-    # --skip-invalid-size-inputs-valid-test \
-    # --max-sentences 20 \
